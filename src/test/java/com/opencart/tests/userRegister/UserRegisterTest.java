a243257401a3bc65669706922816a85e25d1ea57
pekpytxd <akatev58@gmail.com>
2020-11-18 21:27:45 +0200

    Merged all register tests in one class

--- src/test/java/com/opencart/tests/userRegister/UserRegisterTest.java
+++ src/test/java/com/opencart/tests/userRegister/UserRegisterTest.java
@@ -4,7 +4,6 @@ import com.opencart.navigation.Navigation;
 import com.opencart.steps.MainPageBL;
 import com.opencart.steps.RegisterPageBL;
 import com.opencart.tests.BaseTest;
-import org.junit.After;
 import org.testng.annotations.Test;
 
 import static com.opencart.enums.URLs.BASE_URL;
@@ -21,4 +20,61 @@ public class UserRegisterTest extends BaseTest {
                 .registerNewPerson();
         registerPageBL.verifyUserRegistration();
     }
+
+    @Test
+    public void registerUserWithoutTelephone() {
+        new Navigation().navigateToURrl(BASE_URL.getValue());
+        MainPageBL mainPageBL = new MainPageBL();
+        RegisterPageBL registerPageBL = mainPageBL.getHeaderPageBL()
+                .clickOnMyAccountButton()
+                .clickOnRegisterButton()
+                .regiterNewPersonWithoutTelephone();
+        registerPageBL.verifyTelephoneTextError();
+    }
+
+    @Test
+    public void registerUserWithoutPrivacyPolicy() {
+        new Navigation().navigateToURrl(BASE_URL.getValue());
+        MainPageBL mainPageBL = new MainPageBL();
+        RegisterPageBL registerPageBL = mainPageBL.getHeaderPageBL()
+                .clickOnMyAccountButton()
+                .clickOnRegisterButton()
+                .registerNewPersonWithoutPrivacyPolicy();
+        registerPageBL.verifyPrivatePolicyWarning();
+    }
+
+    @Test
+    public void registerUserWithoutName() {
+        new Navigation().navigateToURrl(BASE_URL.getValue());
+        MainPageBL mainPageBL = new MainPageBL();
+        RegisterPageBL registerPageBL = mainPageBL.getHeaderPageBL()
+                .clickOnMyAccountButton()
+                .clickOnRegisterButton()
+                .registerNewPersonWithoutName();
+        registerPageBL.verifyNameTextError();
+    }
+
+    @Test
+    public void registerUserWithoutLastName() {
+        new Navigation().navigateToURrl(BASE_URL.getValue());
+        MainPageBL mainPageBL = new MainPageBL();
+        RegisterPageBL registerPageBL = mainPageBL.getHeaderPageBL()
+                .clickOnMyAccountButton()
+                .clickOnRegisterButton()
+                .registerNewPersonWithoutLastName();
+        registerPageBL.verifyLastNameTextError();
+    }
+
+    @Test
+    public void registerUserWithoutEmail() {
+        new Navigation().navigateToURrl(BASE_URL.getValue());
+        MainPageBL mainPageBL = new MainPageBL();
+        RegisterPageBL registerPageBL = mainPageBL.getHeaderPageBL()
+                .clickOnMyAccountButton()
+                .clickOnRegisterButton()
+                .registerNewPersonWithoutEmail();
+        registerPageBL.verifyEmailTextError();
+    }
+
+
 }
