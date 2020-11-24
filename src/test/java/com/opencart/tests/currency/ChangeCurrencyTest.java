ce35cb508305d20bd44fb08950195ac0bef1f7f1
Sofiia Levinska <sofialevinska@gmail.com>
2020-11-24 13:11:51 +0200

    @BeforeTest method moved to @Test ChangeCurrencyTest

--- src/test/java/com/opencart/tests/currency/ChangeCurrencyTest.java
+++ src/test/java/com/opencart/tests/currency/ChangeCurrencyTest.java
@@ -6,7 +6,6 @@ import com.opencart.steps.AdminDashboardBL;
 import com.opencart.steps.AdminLoginPageBL;
 import com.opencart.steps.MainPageBL;
 import com.opencart.tests.BaseTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 import java.util.Map;
@@ -15,10 +14,10 @@ import static com.opencart.enums.URLs.ADMIN_BASE_URL;
 import static com.opencart.enums.URLs.BASE_URL;
 
 public class ChangeCurrencyTest extends BaseTest {
-    private Map<String, String> currencies;
 
-    @BeforeClass
-    public void getCurrenciesValuesFromAdminPage() {
+    @Test(description = "Test checks if the cost of all products on the website's main page is correct " +
+            "for currencies that are presented on admin page")
+    public void ChangeCurrencyOnMainPageTest() {
         new Navigation().navigateToURrl(ADMIN_BASE_URL.getValue());
         new AdminLoginPageBL().adminLogin();
         new AdminDashboardBL()
@@ -26,12 +25,7 @@ public class ChangeCurrencyTest extends BaseTest {
                 .clickOnLeftNavigationPanelButton("System")
                 .clickOnLeftNavigationPanelButton("Localisation")
                 .clickOnLeftNavigationPanelButton("Currencies");
-        currencies = new AdminCurrencyPageBL().getCurrencyValues();
-    }
-
-    @Test(description = "Test checks if the cost of all products on the website's main page is correct " +
-            "for currencies that are presented on admin page")
-    public void ChangeCurrencyTest() {
+        Map<String, String> currencies = new AdminCurrencyPageBL().getCurrencyValues();
         new Navigation().navigateToURrl(BASE_URL.getValue());
         new MainPageBL().verifyAllProductsPrices(currencies);
     }
